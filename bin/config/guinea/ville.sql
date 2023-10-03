b35e7a8202bc6bc5cb729a97638774e9f001aa72
pabios <pabios.pabiosoft@gmail.com>
2023-10-03 05:33:55 +0200

    [b-00]:adding sql data

---
+++ bin/config/guinea/ville.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: guinea
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `ville`
+--
+
+LOCK TABLES `ville` WRITE;
+/*!40000 ALTER TABLE `ville` DISABLE KEYS */;
+INSERT INTO `ville` VALUES (1,'Kaloum',NULL,1),(2,'Matam',NULL,1),(3,'Dixinn',NULL,1),(4,'Ratoma',NULL,1),(5,'Matoto',NULL,1),(6,'Kassa',NULL,1),(7,'Coyah',NULL,2),(8,'Dubreka',NULL,2),(9,'Forekariah',NULL,2),(10,'Kindia',NULL,2),(11,'Telimele',NULL,2),(12,'Boke',NULL,3),(13,'Boffa',NULL,3),(14,'Gaoual',NULL,3),(15,'Koundara',NULL,3),(16,'Fria',NULL,3),(17,'Mamou',NULL,4),(18,'Dalaba',NULL,4),(19,'Pita',NULL,4),(20,'Labe',NULL,5),(21,'Lelouma',NULL,5),(22,'Tougue',NULL,5),(23,'Mali',NULL,5),(24,'Koubia',NULL,5),(25,'Kankan',NULL,6),(26,'Kerouane',NULL,6),(27,'Kouroussa',NULL,6),(28,'Mandiana',NULL,6),(29,'Siguiri',NULL,6),(30,'Faranah',NULL,7),(31,'Dabola',NULL,7),(32,'Dinguiraye',NULL,7),(33,'Kissidougou',NULL,7),(34,'NZerekore',NULL,8),(35,'Beyla',NULL,8),(36,'Gueckedou',NULL,8),(37,'Lola',NULL,8),(38,'Macenta',NULL,8),(39,'Yomou',NULL,8);
+/*!40000 ALTER TABLE `ville` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-02 10:06:17
